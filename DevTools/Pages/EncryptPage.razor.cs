2b9f847e5c26d852aec6f4d1b2055858d4f2d2d1
Lucas Zottis <zottis.lucas@hotmail.com>
2023-10-28 22:24:15 -0300

    Implementado tela

---
+++ DevTools/Pages/EncryptPage.razor.cs
@@ -0,0 +1,30 @@
+﻿using BibliotecaPublica.Core.Enums;
+using BibliotecaPublica.Utils.Helpers;
+using Microsoft.AspNetCore.Components;
+
+namespace DevTools.Pages
+{
+    public partial class EncryptPage : ComponentBase
+    {
+        private CryptProvider _provider;
+        private string? _input;
+        private string? _output;
+        private string? _keyWord;
+        private bool _crypt = true;
+
+        protected async override Task OnInitializedAsync() { }
+
+        private async Task Execute()
+        {
+            if (_crypt)
+                _output = CryptHelper.Encrypt(_input, _keyWord, _provider);
+            else
+                _output = CryptHelper.Decrypt(_input, _keyWord, _provider);
+        }
+
+        private async Task OnClickButton()
+        {
+            await Execute();
+        } 
+    }
+}
